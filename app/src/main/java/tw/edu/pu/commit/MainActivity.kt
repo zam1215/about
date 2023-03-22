91472b1e2ea38421f6724c774c4369d60761d0aa
natalia <o1103004@gm.pu.edu.tw>
2023-03-22 14:24:06 +0800

    v.1(鍾愛美）

--- app/src/main/java/tw/edu/pu/commit/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/commit/MainActivity.kt
@@ -2,10 +2,20 @@ package tw.edu.pu.commit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var txv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv=findViewById(R.id.txv)
+        txv.setOnClickListener({
+            if(txv.text=="愛美你好"){
+                txv.text="海清班很棒"
+            }else{
+                txv.text="愛美你好"
+            }
+        })
     }
 }
